deca3edd89628ea405f2b629e8156a250cf06d76
毛立鹏 <maolipeng@MacBook-Pro-3.local>
2020-08-25 15:54:33 +0800

    feat: 支持自定义列导出

--- src/views/Table/index.jsx
+++ src/views/Table/index.jsx
@@ -6,12 +6,14 @@ import { export_json_to_excel } from '@/utils/export2Excel'
 import { parseTime } from '@/utils'
 
 import { TableStyle } from './style.js'
-
+let orginData = []
+let orginColumns = []
 const Table = (props) => {
   const [exportName, setExportName] = useState('export-list')
   const [tableData, setTableData] = useState([])
   const [columns, setColumns] = useState([])
   const [reqParams, setReqParams] = useState('')
+  const [columnsStateMap, setColumnsStateMap] = useState({})
   const uploadSuccessFn = (excelData) => {
     console.log('excelData1', excelData)
     const { header, results } = excelData
@@ -21,30 +23,57 @@ const Table = (props) => {
       copyable: true,
     }))
     console.log('headerColumns', headerColumns)
+    orginColumns = headerColumns
     setColumns(headerColumns)
     // const tableList = results.map(d => ({
     //   ...d,
     //   key: d['合同编号']
     // }))
+    orginData = results
+
     setTableData(results)
   }
   const beforeUpload = (files) => {
     console.log('files', files)
     return true
   }
-  const requestList = () => {
+  const requestList = (params) => {
+    console.log('params', params)
+    const data = filterDataFn(params)
+
     return new Promise((resolve, reject) => {
       console.log('tableData', tableData)
       setTimeout(() => {
         resolve({
-          data: tableData,
+          data,
           success: true,
         })
       }, 1000)
     })
   }
+  const postData = (data) => {
+    setTableData(data)
+    return data
+  }
+  const filterDataFn = (params) => {
+    const paramList = Object.keys(params)
+    return orginData.filter((item) => {
+      return paramList.every((p) => {
+        if (p === 'current' || p === 'pageSize') {
+          return true
+        } else {
+          return params[p] === item[p]
+        }
+      })
+    })
+  }
   const exportExcel = () => {
-    const tHeader = columns.map((item) => item.title)
+    const headerList = columns.map((item) => item.title)
+    const HideColumns = Object.keys(columnsStateMap).map(
+      (item) => headerList[item]
+    )
+    console.log('HideColumns', HideColumns)
+    const tHeader = headerList.filter((item) => !HideColumns.includes(item))
     const list = tableData
     const data = formatJson(tHeader, list)
     export_json_to_excel({
@@ -62,6 +91,10 @@ const Table = (props) => {
         }
       })
     )
+  }
+  const setColumnsShow = (map) => {
+    console.log('map', map)
+    setColumnsStateMap(map)
   }
   return (
     <TableStyle>
@@ -93,7 +126,10 @@ const Table = (props) => {
             // defaultData={tableData}
             rowKey="合同编号"
             params={reqParams}
+            columnsStateMap={columnsStateMap}
+            onColumnsStateChange={(map) => setColumnsShow(map)}
             request={requestList}
+            postData={postData}
           ></ProTable>
         )}
       </div>
