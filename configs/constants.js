e24446b1f0e32fdadf867e91d0fce19124aee33c
毛立鹏 <maolipeng@maolipengdeMacBook-Pro-2.local>
2020-08-29 07:48:09 +0800

    chore: 升级构建脚本，支持私钥登录

---
+++ configs/constants.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const path = require('path')
+const privateKey = path.resolve(__dirname, '../../')
+console.log('privateKey', privateKey)
+const DEPLOY_CONFIG = {
+  // develop: {
+  //   host: '10.1.10.244',
+  //   port: 2822,
+  //   username: 'realai',
+  //   password: 'Realai@2020',
+  //   buildPathServer: '/home/realai/deepreal_private/deepreal-fe/dist',
+  // },
+  // test: {
+  //   host: '182.92.185.94',
+  //   port: 2822,
+  //   username: 'root',
+  //   password: 'cbn3YLhihjcGvKKg',
+  //   buildPathServer: '/root/deep-real-1.0/dist',
+  //
+  // },
+  report: {
+    host: '35.220.236.194',
+    port: '22',
+    username: 'gcp',
+    privateKey: fs.readFileSync('/Users/maolipeng/.ssh/gcp-report'),
+    buildPathServer: '/www/report-dist/dist',
+  },
+}
+module.exports = { DEPLOY_CONFIG }
--- configs/proxy.js
+++ configs/proxy.js
@@ -1,4 +1,8 @@
 module.exports = {
+  '/df': {
+    target: ' http://10.1.10.244',
+    changeOrigin: true,
+  },
   '/mock': {
     target: 'http://rap2.taobao.org:38080/app/mock/248309',
     changeOrigin: true,
